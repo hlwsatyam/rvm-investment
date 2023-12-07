8b357277f50d1bde92759e142ca70a1176cbfb43
satyam kumar <satyampandit021@gmail.com>
2023-12-07 10:42:43 +0530

    DB Updated

--- DBConnect/DBConnection.js
+++ DBConnect/DBConnection.js
@@ -1,13 +1,14 @@
 const mongoose = require("mongoose");
-require("dotenv").config({ path: "../.env" });
+
 const DBConnection = async () => {
   await mongoose
     .connect(
       "mongodb+srv://Athena:20172522@cluster0.ghheiye.mongodb.net/?retryWrites=true&w=majority"
     )
-    .then(() => console.log("Database is Connected!"))
-    .catch(() => {
-      console.log("Database connection Failed!");
+    .then(() => console.log("DB is Connected!"))
+    .catch((error) => {
+      console.log("Database connection Failed!!");
+      console.log(error);
     });
 };
 
